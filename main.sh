9128963b6b5d6ad3dd39e821259d7cb841a84c24
tousifanaam <64709584+tousifanaam@users.noreply.github.com>
2020-12-26 19:59:32 +0600

    Add files via upload

--- main.sh
+++ main.sh
@@ -2,77 +2,68 @@
 clear
 
 # The random number $(seq 1 100)
-a=$((1 + $RANDOM % 100))
+a=$((1+$RANDOM%100))
 
 manual () {
-n=0; failure=0
+n=0; failure=1
+echo -e "I am having a number between 1 and 100. Can you guess the number? You have as many attempts you need. The shortest amounts of guesses required to predict is what matters.\n"
 while [ $n -eq 0 ];do
-	read -p $'I am having a number between 1 and 100.\nCan you guess the number?\nYou have as many attempts you need.\nThe shortest amounts of guesses required to predict is what matters.\n\nEnter your guess:  >  '  guess
+	read -p "Enter your guess:  >  "  guess
 	if [ $guess = $a ];then
-		b=$(($failure+1))
-		echo -e "\nCorrect guess!. No. of attempts taken: $b"
+		echo -e "\nCorrect guess!. No. of attempts taken: $failure"
 		n=1
 	elif [ $guess -gt $a ];then
-		echo -e "\nToo high!\n"
+		m='Too High'
 		failure=$(($failure+1))
 	elif [ $guess -lt $a ];then
-		echo -e "\nToo low!\n"
+		m='Too Low!'
 		failure=$(($failure+1))
 	fi
-done 
+	if [ $n -eq 0 ];then
+		sleep 1s && clear
+		if [ $failure -eq 2 ];then
+			echo -e "Last Guess: $guess | $m | ${failure}nd attempt.\n"
+		elif [ $failure -eq 3 ];then
+			echo -e "Last Guess: $guess | $m | ${failure}rd attempt.\n"
+		else
+			echo -e "Last Guess: $guess | $m | ${failure}th attempt.\n"
+		fi
+	fi
+done
 }
 
 auto () {
-guess=50; failure=0; ll=1, ul=100
+# variables
+low=1; high=100; guess=50; attepmt=0
 
-# Start
-if [ $guess -eq $a ];then
-	attempts=$(($failure+1))
-	echo -e "\n${guess} - Correct guess!. No. of attempts taken: $attempts"
-	exit
-elif [ $guess -gt $a ];then
-	echo -e "\n${guess} - Too High!" # -
-	pg2=$guess; failure=$(($failure+1))
-	guess=$((${ul}/4))
-elif [ $guess -lt $a ];then
-	echo -e "\n${guess} - Too Low!" # +
-	pg2=$guess; failure=$(($failure+1))
-	guess=$(($guess+$((${guess}/2))))
-fi
-pg1=$guess # output variables: guess = 25 or 75, pg1 = 25 or 75, pg2 = 50
-
-# Main Process 
-check () {
-if [ $guess -eq $a ];then
-	attempts=$(($failure+1))
-	echo -e "\n${guess} - Correct guess!. No. of attempts taken: $attempts"; n=1
-	exit
-elif [ $guess -gt $a ];then 
-	echo -e "\n${guess} - Too High!" # -
-	if [ $pg2 -gt $pg1 ];then # previous guess was an output of too high
-		pg3=$pg2; pg2=$pg1; failure=$(($failure+1)) 
-		guess=$(($(($ll+$pg2))/2))
-	elif [ $pg2 -lt $pg1 ];then # previous guess was an output of too low
-		pg3=$pg2; pg2=$pg1; failure=$(($failure+1)) 
-		guess=$(($(($pg3+$pg2))/2))
+# Main
+while :; do
+	attempt=$((attempt+1))
+	if [ $guess -eq $a ];then
+		if [ $guess -lt 10 ];then
+			echo "0$guess - Done!"
+		else
+			echo "$guess - Done!"
+		fi
+		echo -e "\nTotal attempts: $attempt\nSuccess!";break
+	elif [ $guess -eq 99 ] && [ $guess -lt $a ];then
+		echo "$guess - Too low!"
+		guess=$(($guess+1))
+	elif [ $guess -lt $a ];then
+		if [ $guess -lt 10 ];then
+			echo "0$guess - Too low!"
+		else
+			echo "$guess - Too low!"
+		fi
+		low=$guess; guess=$((($high+$low)/2))
+	elif [ $guess -gt $a ];then
+		if [ $guess -lt 10 ];then
+			echo "0$guess - Too low!"
+		else
+			echo "$guess - Too low!"
+		fi
+		high=$guess; guess=$((($high+$low)/2))
 	fi
-elif [ $guess -lt $a ];then
-	echo -e "\n${guess} - Too Low!" # +
-	if [ $pg2 -gt $pg1 ];then # previous guess was an output of too high
-		pg3=$pg2; pg2=$pg1; failure=$(($failure+1)) 
-		guess=$(($(($pg2+$pg3))/2))
-	elif [ $pg2 -lt $pg1 ];then # previous guess was an output of too low
-		pg3=$pg2; pg2=$pg1; failure=$(($failure+1)) 
-		guess=$(($(($pg2+$ul))/2))
-	fi	
-fi 
-pg1=$guess 
-}
-
-# Loop
-n=0
-while [ $n -eq 0 ];do
-	check
 done
 }
 
@@ -80,7 +71,7 @@ thread () {
 failure=0
 for i in $(seq 1 100);do
 	if [ $i -lt $a ];then
-		echo -e "\n${i} - Too low!"
+		echo -e "${i} - Too low!"
 		failure=$(($failure+1))
 	elif [ $i = $a ];then
 		b=$(($failure+1))
